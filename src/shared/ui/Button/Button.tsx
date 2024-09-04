f7e5ebaaacb8b366fc7c40e5a5228342c1eff12b
WarGhost132 <wawghostick@gmail.com>
2024-09-04 18:29:54 +0300

    add react testing lib, add tests for btn, sidebar

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,5 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { ButtonHTMLAttributes, FC } from 'react';
+// @ts-ignore
 import cls from './Button.module.scss';
 
 export enum ThemeButton {
