242a25ed20f24f09b044565cf19f457f5a01c763
WarGhost132 <wawghostick@gmail.com>
2024-09-05 13:27:59 +0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttributes: ['data-testid'] }],
+        'i18next/no-literal-string': 'off',
         'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
@@ -50,9 +50,6 @@ module.exports = {
     overrides: [
         {
             files: ['**/src/**/*.test.{ts,tsx}'],
-            rules: {
-                'i18next/no-literal-string': 'off',
-            }
-        }
-    ]
+        },
+    ],
 };
