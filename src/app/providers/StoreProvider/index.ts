c82494dd72914530d2873bbdfdcda089bada5f16
WarGhost132 <wawghostick@gmail.com>
2024-09-14 17:01:07 +0300

    add lazy loading, add reducer manager, add dynamic module loader component

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,9 +1,10 @@
 import { StoreProvider } from 'app/providers/StoreProvider/ui/StoreProvider';
-import type { StateSchema } from './config/StateSchema';
+import type { StateSchema, ReduxStoreWithManager } from './config/StateSchema';
 import { createReduxStore } from './config/store';
 
 export {
     StoreProvider,
     createReduxStore,
     StateSchema,
+    ReduxStoreWithManager,
 };
