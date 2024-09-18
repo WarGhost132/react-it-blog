844ee7463bcbefbedbe85c5adadb6b1d77ad3727
WarGhost132 <wawghostick@gmail.com>
2024-09-18 17:39:37 +0300

    update profile, private routes, ui select, country select, currency select

---
+++ src/shared/ui/Select/Select.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Select } from 'shared/ui/Select/Select';
+
+export default {
+    title: 'shared/Select',
+    component: Select,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: {
+        to: '/',
+    },
+} as ComponentMeta<typeof Select>;
+
+const Template: ComponentStory<typeof Select> = (args) => <Select {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    label: 'Укажите значение',
+    options: [
+        { value: '1', content: 'Первый' },
+        { value: '2', content: 'Второй' },
+        { value: '3', content: 'Третий' },
+    ],
+};
