81c5f743b1a3741a46fc612a9b91855fd7f852c5
WarGhost132 <wawghostick@gmail.com>
2024-09-19 17:12:35 +0300

    add react refresh plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,22 +1,28 @@
+// @ts-ignore
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
+// @ts-ignore
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import {
+    DefinePlugin, HotModuleReplacementPlugin, ProgressPlugin, WebpackPluginInstance,
+} from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+// @ts-ignore
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
     paths, isDev, apiUrl, project,
-}: BuildOptions): webpack.WebpackPluginInstance[] {
+}: BuildOptions): WebpackPluginInstance[] {
     const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
-        new webpack.ProgressPlugin(),
+        new ProgressPlugin(),
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
-        new webpack.DefinePlugin({
+        new DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
             __API__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project),
@@ -24,7 +30,8 @@ export function buildPlugins({
     ];
 
     if (isDev) {
-        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new ReactRefreshWebpackPlugin());
+        plugins.push(new HotModuleReplacementPlugin());
         plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false,
         }));
