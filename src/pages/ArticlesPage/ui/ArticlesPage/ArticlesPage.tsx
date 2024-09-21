e47878ad5e293d8863bad11d8abaff02892e260a
WarGhost132 <wawghostick@gmail.com>
2024-09-21 16:44:07 +0300

    add skeleton, code, article details

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -11,9 +11,7 @@ const ArticlesPage = (props: ArticlesPageProps) => {
     const { className } = props;
     const { t } = useTranslation('articles');
     return (
-        <div className={classNames(cls.ArticlesPage, {}, [className])}>
-            Articles
-        </div>
+        <div className={classNames(cls.ArticlesPage, {}, [className])} />
     );
 };
 
