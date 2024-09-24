ca73e6ea1f37156c561883f9ffd6b92de877934a
WarGhost132 <wawghostick@gmail.com>
2024-09-24 16:48:44 +0300

    fix tests

--- src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {ComponentStory, ComponentMeta} from '@storybook/react';
-import {ArticleViewSelector} from './ArticleViewSelector';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ArticleViewSelector } from './ArticleViewSelector';
 
 export default {
     title: 'shared/ArticleViewSelector',
     component: ArticleViewSelector,
     argTypes: {
-        backgroundColor: {control: 'color'},
+        backgroundColor: { control: 'color' },
     },
 } as ComponentMeta<typeof ArticleViewSelector>;
 
