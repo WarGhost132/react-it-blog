a9207d6d7e775ddb4ac99785aabb7346f616cd18
WarGhost132 <wawghostick@gmail.com>
2024-09-27 18:01:45 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -5,7 +5,7 @@ export const $api = axios.create({
     baseURL: __API__,
 });
 
-$api.interceptors.response.use((config) => {
+$api.interceptors.request.use((config) => {
     if (config.headers) {
         config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
     }
