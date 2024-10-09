d8450c8a816e24b9b07d5f9f7f841b0a647ae975
WarGhost132 <wawghostick@gmail.com>
2024-10-09 14:28:38 +0300

    add popover, add notifications

---
+++ src/entities/Notification/ui/NotificationList/NotificationList.tsx
@@ -0,0 +1,44 @@
+import { memo } from 'react';
+import { VStack } from 'shared/ui/Stack';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
+import { NotificationItem } from '../NotificationItem/NotificationItem';
+import { useNotifications } from '../../api/notificationApi';
+import cls from './NotificationList.module.scss';
+
+interface NotificationListProps {
+    className?: string;
+}
+
+export const NotificationList = memo((props: NotificationListProps) => {
+    const { className } = props;
+    const { data, isLoading } = useNotifications(null, {
+        pollingInterval: 10000,
+    });
+
+    if (isLoading) {
+        return (
+            <VStack
+                gap="16"
+                max
+                className={classNames(cls.NotificationList, {}, [className])}
+            >
+                <Skeleton width="100%" border="8px" />
+                <Skeleton width="100%" border="8px" />
+                <Skeleton width="100%" border="8px" />
+            </VStack>
+        );
+    }
+
+    return (
+        <VStack
+            gap="16"
+            max
+            className={classNames(cls.NotificationList, {}, [className])}
+        >
+            {data?.map((item) => (
+                <NotificationItem key={item.id} item={item} />
+            ))}
+        </VStack>
+    );
+});
---
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
@@ -0,0 +1,60 @@
+import React, { memo, useCallback } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Dropdown } from 'shared/ui/Popups';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    getUserAuthData, isUserAdmin, isUserManager, userActions,
+} from 'entities/User';
+import cls from './AvatarDropdown.module.scss';
+
+interface AvatarDropdownProps {
+    className?: string;
+}
+
+export const AvatarDropdown = memo((props: AvatarDropdownProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+    const authData = useSelector(getUserAuthData);
+    const dispatch = useDispatch();
+    const isAdmin = useSelector(isUserAdmin);
+    const isManager = useSelector(isUserManager);
+
+    const onLogout = useCallback(() => {
+        dispatch(userActions.logout());
+    }, [dispatch]);
+
+    const isAdminPanelAvailable = isAdmin || isManager;
+
+    if (!authData) {
+        return null;
+    }
+
+    return (
+        <Dropdown
+            className={classNames(cls.AvatarDropdown, {}, [className])}
+            direction="bottom left"
+            items={[
+                ...(isAdminPanelAvailable
+                    ? [
+                        {
+                            content: t('admin'),
+                            href: RoutePath.admin_panel,
+                        },
+                    ]
+                    : []),
+                {
+                    content: t('Профиль'),
+                    href: RoutePath.profile + authData.id,
+                },
+                {
+                    content: t('Выйти'),
+                    onClick: onLogout,
+                },
+            ]}
+            trigger={<Avatar size={30} src={authData.avatar} />}
+        />
+    );
+});
