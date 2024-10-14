9a1a520ae93dca2d93856a9ac19cd008dadcab84
WarGhost132 <wawghostick@gmail.com>
2024-10-14 13:59:18 +0300

    update custom eslint plugin, add support for alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -57,7 +57,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
-        'warghost-plugin/path-checker': 'error',
+        'warghost-plugin/path-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
