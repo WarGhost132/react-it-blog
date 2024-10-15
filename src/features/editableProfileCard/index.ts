e244a53bd69d4e796302a0742dfcf9c9a4a76dac
WarGhost132 <wawghostick@gmail.com>
2024-10-15 13:07:39 +0300

    remove ProfilePageHeader

--- src/features/editableProfileCard/index.ts
+++ src/features/editableProfileCard/index.ts
@@ -1,9 +1,2 @@
 export { EditableProfileCard } from './ui/EditableProfileCard/EditableProfileCard';
 export type { ProfileSchema } from './model/types/editableProfileCardSchema';
-
-export { getProfileData } from './model/selectors/getProfileData/getProfileData';
-export { getProfileReadonly } from './model/selectors/getProfileReadonly/getProfileReadonly';
-
-export { updateProfileData } from './model/services/updateProfileData/updateProfileData';
-
-export { profileActions } from './model/slice/profileSlice';
