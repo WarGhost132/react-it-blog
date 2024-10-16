7724fda496f87c728061db9102f86b0092fa3606
WarGhost132 <wawghostick@gmail.com>
2024-10-16 12:22:54 +0300

    refactor public api for shared ui

--- src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
+++ src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
-import { Card, CardTheme } from '@/shared/ui/Card/Card';
-import { Text } from '@/shared/ui/Text/Text';
+import { Card, CardTheme } from '@/shared/ui/Card';
+import { Text } from '@/shared/ui/Text';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Notification } from '../../model/types/notifications';
 import cls from './NotificationItem.module.scss';
