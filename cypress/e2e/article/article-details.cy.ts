84751d79a97d74e7fc24d9bdbd1b56109aee1522
WarGhost132 <wawghostick@gmail.com>
2024-10-22 11:47:58 +0300

    skip one test, add stubs (fixtures)

--- cypress/e2e/article/article-details.cy.ts
+++ cypress/e2e/article/article-details.cy.ts
@@ -11,22 +11,33 @@ describe('Пользователь заходит на страницу стат
     afterEach(() => {
         cy.removeArticle(currentArticleId);
     });
-    it('И видит содержимое статьи', () => {
-        cy.getByTestId('ArticleDetails.Info').should('exist');
+    describe('Работа с API', () => {
+        it('И видит содержимое статьи', () => {
+            cy.getByTestId('ArticleDetails.Info').should('exist');
+        });
+        it('И видит список рекомендаций', () => {
+            cy.getByTestId('ArticleRecommendationsList').should('exist');
+        });
+        it('И оставляет комментарий', () => {
+            cy.getByTestId('ArticleDetails.Info');
+            cy.getByTestId('AddCommentForm').scrollIntoView();
+            cy.addComment('text');
+            cy.getByTestId('CommentCard.Content').should('have.length', 1);
+        });
+        it('И ставит оценку', () => {
+            cy.getByTestId('ArticleDetails.Info');
+            cy.getByTestId('RatingCard').scrollIntoView();
+            cy.setRate(5, 'feedback');
+            cy.get('[data-selected=true]').should('have.length', 5);
+        });
     });
-    it('И видит список рекомендаций', () => {
-        cy.getByTestId('ArticleRecommendationsList').should('exist');
-    });
-    it('И оставляет комментарий', () => {
-        cy.getByTestId('ArticleDetails.Info');
-        cy.getByTestId('AddCommentForm').scrollIntoView();
-        cy.addComment('text');
-        cy.getByTestId('CommentCard.Content').should('have.length', 1);
-    });
-    it('И ставит оценку', () => {
-        cy.getByTestId('ArticleDetails.Info');
-        cy.getByTestId('RatingCard').scrollIntoView();
-        cy.setRate(5, 'feedback');
-        cy.get('[data-selected=true]').should('have.length', 5);
+    describe('Работа на фикстурах', () => {
+        it('И ставит оценку (пример с стабом на фикстурах)', () => {
+            cy.intercept('GET', '**/articles/*', { fixture: 'article-details.json' });
+            cy.getByTestId('ArticleDetails.Info');
+            cy.getByTestId('RatingCard').scrollIntoView();
+            cy.setRate(5, 'feedback');
+            cy.get('[data-selected=true]').should('have.length', 5);
+        });
     });
 });
