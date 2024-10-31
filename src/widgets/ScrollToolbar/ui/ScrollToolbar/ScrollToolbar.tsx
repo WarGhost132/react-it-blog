fd85f14b525a0687bdf4a5b5bd1c2a69efa99a07
WarGhost132 <wawghostick@gmail.com>
2024-10-31 17:52:16 +0300

    scrollToolbar widget, useAppToolbar, useRoute change

---
+++ src/widgets/ScrollToolbar/ui/ScrollToolbar/ScrollToolbar.tsx
@@ -0,0 +1,16 @@
+import { memo } from 'react';
+import { VStack } from '@/shared/ui/redesigned/Stack';
+import { ScrollToTopButton } from '@/features/scrollToTopButton';
+
+interface ScrollToolbarProps {
+    className?: string;
+}
+
+export const ScrollToolbar = memo((props: ScrollToolbarProps) => {
+    const { className } = props;
+    return (
+        <VStack justify="center" align="center" max className={className}>
+            <ScrollToTopButton />
+        </VStack>
+    );
+});
